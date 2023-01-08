488e0415290aa856cf38284b6e0f30d978123b58
Kosynskyi <kosynskyi.vova@gmail.com>
2023-01-08 18:29:20 +0200

    fix refresh and add styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -4,9 +4,15 @@ import { toast } from 'react-toastify';
 import { fetchAllProducts, fetchProductById } from 'services/API';
 import { ProductTableItem } from 'components/ProductTableItem';
 import { Box } from 'components/Box';
-import { StyledLabel, StyledInput } from './Filter.styled';
+import {
+  StyledLabel,
+  StyledInput,
+  StyledButton,
+  StyledForm,
+} from './Filter.styled';
 
 export const Filter = () => {
+  const [filter, setFilter] = useState(0);
   const [totalProducts, setTotalProducts] = useState(null);
   const [prodItem, setProdItem] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
@@ -22,9 +28,10 @@ export const Filter = () => {
   }, []);
 
   const handleChange = e => {
-    if (Number(e.target.value) < 1) {
+    if (Number(e.target.value) < 1 && e.target.value !== '') {
+      console.log(e.target.value);
       toast.error('id cannot be less then 1 :( please, try again');
-      setSearchParams({ query: 0 });
+      setSearchParams({ query: filter });
     } else if (Number(e.target.value) > totalProducts) {
       setSearchParams({ query: totalProducts });
       toast.error(
@@ -32,6 +39,7 @@ export const Filter = () => {
       );
     } else {
       setSearchParams({ query: e.target.value });
+      setFilter(e.target.value);
     }
   };
 
@@ -39,24 +47,36 @@ export const Filter = () => {
     e.preventDefault();
     e.target.reset();
     setSearchParams({ query: 0 });
+    setFilter(0);
   };
 
   return (
     <>
-      <Box display="flex" alignItems="flex-end" justifyContent="center" mb={6}>
-        <form onSubmit={handleSubmit}>
+      <Box display="flex" alignItems="flex-end" justifyContent="center" mb={5}>
+        <StyledForm onSubmit={handleSubmit}>
           <StyledLabel>
             <span>Find product by id</span>
             {'   '}
+
             <StyledInput
               type="number"
               name="filter"
-              placeholder="enter id (only numbers)"
+              aria-label="enter product id"
+              placeholder="id"
+              value={query ? query : filter}
               onChange={handleChange}
             />
           </StyledLabel>
-          <button disabled={!query === null}>Reset</button>
-        </form>
+          <Box ml={5}>
+            {' '}
+            <StyledButton
+              aria-label="reset"
+              disabled={!Number(query) && !Number(filter)}
+            >
+              Reset
+            </StyledButton>
+          </Box>
+        </StyledForm>
       </Box>
       {query > 0 && (
         <ProductTableItem prodItem={prodItem} totalProducts={totalProducts} />
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -2,12 +2,38 @@ import styled from 'styled-components';
 
 export const StyledLabel = styled.label`
   display: inline-block;
-
   font-size: ${props => props.theme.fontSizes.m};
   font-weight: 600;
-  margin-right: ${props => props.theme.space[4]};
 `;
 
 export const StyledInput = styled.input`
-  margin-left: ${props => props.theme.space[4]};
+  font-size: ${props => props.theme.fontSizes.s};
+  width: 45px;
+`;
+
+export const StyledButton = styled.button`
+  padding: ${props => props.theme.space[4]};
+  border: ${props => props.theme.borders.normal};
+  border-radius: ${props => props.theme.radii.awesome};
+  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.white};
+  letter-spacing: 0.1rem;
+  width: 70px;
+  transition: 500ms;
+
+  &:disabled {
+    background-color: ${props => props.theme.colors.muted};
+    color: ${props => props.theme.colors.black};
+    border: ${props => props.theme.borders.none};
+  }
+
+  &:hover:not(:disabled) {
+    background-color: ${props => props.theme.colors.secondary};
+    transform: scale(1.05);
+  }
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  align-items: center;
 `;
